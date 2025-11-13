cefe8beb146c8e154cbb928e02247a3dacb9007e
emguan <eguan3@jh.edu>
2025-11-12 20:42:24 -0500

    update to barycentric algo

--- utils/triangles.py
+++ utils/triangles.py
@@ -1,31 +1,38 @@
 import numpy as np
-from utils.mathpackage import find_area
+import math
 
 class Triangle:
     def __init__(self, pt1, pt2, pt3):
-        self.p1 = np.array(pt1, dtype=float)
-        self.p2 = np.array(pt2, dtype=float)
-        self.p3 = np.array(pt3, dtype=float)
-        self.area = find_area(self.p1, self.p2, self.p3)
+        self.a = np.array(pt1, dtype=float)
+        self.b = np.array(pt2, dtype=float)
+        self.c = np.array(pt3, dtype=float)
 
-    def find_u(self, p):
-        return find_area(p, self.p2, self.p3) / self.area
-
-    def find_v(self, p):
-        return find_area(p, self.p1, self.p3) / self.area
-
-    def find_w(self, p):
-        return find_area(p, self.p1, self.p2) / self.area
+        self.normal = np.cross(self.b - self.a, self.c - self.a)
+        self.normal = self.normal / np.linalg.norm(self.normal)
 
     def barycentric_coords(self, p):
-        u = self.find_u(p)
-        v = self.find_v(p)
-        w = 1 - u - v
+        v0 = self.b - self.a
+        v1 = self.c - self.a
+        v2 = p - self.a
+
+        d00 = np.dot(v0, v0)
+        d01 = np.dot(v0, v1)
+        d10 = np.dot(v1, v0)
+        d11 = np.dot(v1, v1)
+        d20 = np.dot(v2, v0)
+        d21 = np.dot(v2, v1)
+
+        denom = d00 * d11 - d01 * d01
+
+        v = (d11 * d20 - d01 * d21) / denom
+        w = (d00 * d21 - d01 * d20) / denom
+        u = 1.0 - v - w
         return u, v, w
 
-    def in_triangle(self, p):
-        u, v, w = self.barycentric_coords(p)
-        return (u >= 0) and (v >= 0) and (w >= 0)
+    def project_to_plane(self, p):
+        p = np.array(p, dtype=float)
+        dist = np.dot(p - self.a, self.normal)
+        return p - dist * self.normal
 
     def closest_point_on_edge(self, p, a, b):
         ab = b - a
@@ -35,15 +42,24 @@ class Triangle:
 
     def closest_point(self, p):
         p = np.array(p, dtype=float)
-        if self.in_triangle(p):
-            return p
 
-        u, v, w = self.barycentric_coords(p)
-        if u < 0:
-            return self.closest_point_on_edge(p, self.p2, self.p3)
-        elif v < 0:
-            return self.closest_point_on_edge(p, self.p1, self.p3)
-        elif w < 0:
-            return self.closest_point_on_edge(p, self.p1, self.p2)
-        else:
-            raise RuntimeError("Unexpected geometry condition")
+        # 1. Project p onto triangle plane
+        p_proj = self.project_to_plane(p)
+
+        # 2. Barycentric of projection
+        u, v, w = self.barycentric_coords(p_proj)
+
+        # 3. If inside: return projection
+        if (u >= 0) and (v >= 0) and (w >= 0):
+            return p_proj
+
+        # 4. Outside: find edge closest point using 3D edges
+        c1 = self.closest_point_on_edge(p, self.a, self.b)
+        c2 = self.closest_point_on_edge(p, self.b, self.c)
+        c3 = self.closest_point_on_edge(p, self.c, self.a)
+
+        # 5. Choose point closest to original p (not p_proj!)
+        candidates = [c1, c2, c3]
+        dists = [np.linalg.norm(p - c) for c in candidates]
+
+        return candidates[np.argmin(dists)]
